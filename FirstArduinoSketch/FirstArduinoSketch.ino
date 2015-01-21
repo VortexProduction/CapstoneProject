ab7e4b5b8ae07fd0e95018503e8aaab03983d494
jfried92 <jfried92@gmail.com>
2015-01-21 16:07:47 -0700

    Jan 21 Changes

--- FirstArduinoSketch/FirstArduinoSketch.ino
+++ FirstArduinoSketch/FirstArduinoSketch.ino
@@ -1,11 +1,55 @@
 // include the library code:
 #include <LiquidCrystal.h>
 #include <Wire.h>
- 
+#include <Adafruit_GPS.h>
+#include <SoftwareSerial.h>
+
+
+SoftwareSerial mySerial(3, 2);
+LiquidCrystal lcd(7, 8, 9, 12, 4, 13);
+
+
+Adafruit_GPS GPS(&mySerial);
+
+
+#define GPSECHO  true
+
+boolean usingInterrupt = false;
+void useInterrupt(boolean); // Func prototype keeps Arduino 0023 happy
+
+
+// Interrupt is called once a millisecond, looks for any new GPS data, and stores it
+SIGNAL(TIMER0_COMPA_vect) {
+  char c = GPS.read();
+  // if you want to debug, this is a good time to do it!
+#ifdef UDR0
+  if (GPSECHO)
+    if (c) UDR0 = c;  
+    // writing direct to UDR0 is much much faster than Serial.print 
+    // but only one character can be written at a time. 
+#endif
+}
+
+void useInterrupt(boolean v) {
+  if (v) {
+    // Timer0 is already used for millis() - we'll just interrupt somewhere
+    // in the middle and call the "Compare A" function above
+    OCR0A = 0xAF;
+    TIMSK0 |= _BV(OCIE0A);
+    usingInterrupt = true;
+  } else {
+    // do not call the interrupt function COMPA anymore
+    TIMSK0 &= ~_BV(OCIE0A);
+    usingInterrupt = false;
+  }
+}
+
+uint32_t timer = millis();
+
 #define BLUELITE 11
  
 // initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(7, 8, 9, 12, 4, 2);
+
  
 // you can change the overall brightness by range 0 -> 255
 int brightness = 255;
@@ -16,7 +60,7 @@ void setup() {
   // set up the LCD's number of rows and columns: 
   lcd.begin(16, 2);
   // Print a message to the LCD.
-  lcd.print("HashtagYoloSwag");
+  
   
   pinMode(BLUELITE, OUTPUT);
   pinMode(10,OUTPUT);    //Yellow
@@ -25,13 +69,46 @@ void setup() {
   pinMode(5,OUTPUT);     //Blue
 
   brightness = 100;
+  
+      
+  // connect at 115200 so we can read the GPS fast enough and echo without dropping chars
+  // also spit it out
+  Serial.begin(115200);
+  Serial.println("Adafruit GPS library basic test!");
+
+  // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
+  GPS.begin(9600);
+  
+  // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  // uncomment this line to turn on only the "minimum recommended" data
+  //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
+  // For parsing data, we don't suggest using anything but either RMC only or RMC+GGA since
+  // the parser doesn't care about other sentences at this time
+  
+  // Set the update rate
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
+  // For the parsing code to work nicely and have time to sort thru the data, and
+  // print it out we don't suggest using anything higher than 1 Hz
+
+  // Request updates on antenna status, comment out to keep quiet
+  GPS.sendCommand(PGCMD_ANTENNA);
+
+  // the nice thing about this code is you can have a timer0 interrupt go off
+  // every 1 millisecond, and read data from the GPS for you. that makes the
+  // loop code a heck of a lot easier!
+  useInterrupt(true);
+
+  delay(1000);
+  // Ask for firmware version
+  mySerial.println(PMTK_Q_RELEASE);
 }
  
  
 void loop() {
   
   setBacklight(255, 0, 255);
-  analogWrite(3,counter);
+  
   analogWrite(6,counter2);
   counter+= 10;
   counter2+= 20;
@@ -42,8 +119,66 @@ void loop() {
   if(counter2 >= 127)
   {
     counter2 = 50;
+  } 
+  // in case you are not using the interrupt above, you'll
+  // need to 'hand query' the GPS, not suggested :(
+  if (! usingInterrupt) {
+    // read data from the GPS in the 'main loop'
+    char c = GPS.read();
+    // if you want to debug, this is a good time to do it!
+    if (GPSECHO)
+      if (c) Serial.print(c);
   }
- delay(50); 
+  
+  // if a sentence is received, we can check the checksum, parse it...
+  if (GPS.newNMEAreceived()) {
+    // a tricky thing here is if we print the NMEA sentence, or data
+    // we end up not listening and catching other sentences! 
+    // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
+    //Serial.println(GPS.lastNMEA());   // this also sets the newNMEAreceived() flag to false
+  
+    if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
+      return;  // we can fail to parse a sentence in which case we should just wait for another
+  }
+
+  // if millis() or timer wraps around, we'll just reset it
+  if (timer > millis())  timer = millis();
+
+  // approximately every 2 seconds or so, print out the current stats
+  if (millis() - timer > 2000) { 
+    timer = millis(); // reset the timer
+    
+    //Serial.print("\nTime: ");
+    //Serial.print(GPS.hour, DEC); Serial.print(':');
+    //Serial.print(GPS.minute, DEC); Serial.print(':');
+    //Serial.print(GPS.seconds, DEC); Serial.print('.');
+    //Serial.println(GPS.milliseconds);
+    //Serial.print("Date: ");
+    //Serial.print(GPS.day, DEC); Serial.print('/');
+    //Serial.print(GPS.month, DEC); Serial.print("/20");
+    //Serial.println(GPS.year, DEC);
+    //Serial.print("Fix: "); Serial.print((int)GPS.fix);
+    //Serial.print(" quality: "); Serial.println((int)GPS.fixquality); 
+    
+      //Serial.print("Location: ");
+      //Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
+      //Serial.print(", "); 
+      //Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
+      Serial.print("Location (in degrees, works with Google Maps): ");
+      Serial.print(GPS.latitudeDegrees, 4);
+      Serial.print(", "); 
+      Serial.println(GPS.longitudeDegrees, 4);
+      
+      lcd.setCursor(0,0);
+      lcd.print("Speed (knots):");
+      lcd.setCursor(0,1);
+      lcd.print((double)GPS.speed);
+      Serial.print("Speed: ");Serial.println(GPS.speed);
+      Serial.print("Angle: "); Serial.println(GPS.angle);
+      Serial.print("Altitude: "); Serial.println(GPS.altitude);
+      Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
+    
+  }
 }
 
  
@@ -56,7 +191,21 @@ void setBacklight(uint8_t r, uint8_t g, uint8_t b) {
  
   // common anode so invert!
   b = map(b, 0, 255, 255, 0);
-
-  Serial.print(" B = "); Serial.println(b, DEC);
   analogWrite(BLUELITE, b);
 }
+
+
+// Test code for Adafruit GPS modules using MTK3329/MTK3339 driver
+//
+// This code shows how to listen to the GPS module in an interrupt
+// which allows the program to have more 'freedom' - just parse
+// when a new NMEA sentence is available! Then access data when
+// desired.
+//
+// Tested and works great with the Adafruit Ultimate GPS module
+// using MTK33x9 chipset
+//    ------> http://www.adafruit.com/products/746
+// Pick one up today at the Adafruit electronics shop 
+// and help support open source hardware & software! -ada
+
+
